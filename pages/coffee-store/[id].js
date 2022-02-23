92cfa39c7e7df5cf9f6d04c1ce054b36984e436f
Saksham Bindal <2020UCP1801@mnit.ac.in>
2022-02-23 11:40:21 +0530

    coffee store from api

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,32 +1,46 @@
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import cls from "classnames";
 import { useRouter } from "next/router";
-import coffeeStores from "../../data/coffee-store.json";
+// import coffeeStores from "../../data/coffee-store.json";
+import styles from "../../styles/coffee-store.module.css";
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
 export async function getStaticProps({ params }) {
-  console.log(params);
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
     props: {
-      coffeeStore: coffeeStores.find((store) => store.id === Number(params.id)),
+      coffeeStore: coffeeStores.find(
+        (store) => String(store.fsq_id) === params.id
+      ),
     },
   };
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
   // Used for static rendering of dynamic routes
   // Provide all paths to render statically
   // Provide fallback false to give 404 error on paths not included here
   // If fallback is true then on first request it will be loaded and then on further request it got statically stored
+
+  const coffeeStores = await fetchCoffeeStores();
+
+  const paths = coffeeStores.map((store) => {
+    return {
+      params: {
+        id: String(store.fsq_id),
+      },
+    };
+  });
   return {
-    paths: [
-      { params: { id: "0" } },
-      { params: { id: "1" } },
-      { params: { id: "2" } },
-    ],
+    paths,
     fallback: true,
   };
 }
 
-const CoffeeStore = () => {
+const CoffeeStore = (props) => {
   const router = useRouter();
   const { id } = router.query;
 
@@ -34,12 +48,77 @@ const CoffeeStore = () => {
   // So show loading state till then otherwise will give error
   if (router.isFallback) return <div>Loading...</div>;
 
+  const { name, imgUrl, location } = props.coffeeStore;
+
+  const handleUpvoteButton = () => {};
+
   return (
-    <div>
-      <p>{id}</p>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={`${name} coffee store`}></meta>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          ></Image>
+        </div>
+
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/places.svg"
+              width="24"
+              height="24"
+              alt="places icon"
+            />
+            <p className={styles.text}>
+              {location.address || location.formatted_address}
+            </p>
+          </div>
+          {location.neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/nearMe.svg"
+                width="24"
+                height="24"
+                alt="near me icon"
+              />
+              <p className={styles.text}>{location.neighbourhood}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/star.svg"
+              width="24"
+              height="24"
+              alt="star icon"
+            />
+            {/* <p className={styles.text}>{votingCount}</p> */}
+            <p className={styles.text}>{1}</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/Banner/Banner";
 import Card from "../components/Card/Card";
+import { fetchCoffeeStores } from "../lib/coffee-stores";
 import styles from "../styles/Home.module.css";
-import coffeeStores from "../data/coffee-store.json";
+// import coffeeStores from "../data/coffee-store.json";
 
 export async function getStaticProps(context) {
   // This data will be downloaded on build time for static site rendereing\
   // Used for data which does not change dynamically
   // Store api calls which does not change in it
+
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
     props: {
       coffeeStores,
@@ -36,22 +40,25 @@ export default function Home(props) {
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
         {props.coffeeStores.length > 0 && (
-          <>
-            <h2 className={styles.heading2}>Toronto Stores</h2>
+          <div style={{ marginTop: "80px" }}>
+            <h2 className={styles.heading2}>New Delhi Stores</h2>
             <div className={styles.cardLayout}>
               {props.coffeeStores.map((store) => {
                 return (
                   <Card
-                    key={store.id}
+                    key={store.fsq_id}
                     name={store.name}
-                    href={`/coffee-store/${store.id}`}
-                    imgUrl={store.imgUrl}
+                    href={`/coffee-store/${store.fsq_id}`}
+                    imgUrl={
+                      store.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
                     className={styles.card}
                   />
                 );
               })}
             </div>
-          </>
+          </div>
         )}
       </main>
     </div>
